a96798389e9accaa8c9907eadbb5ade6b524111e
Ilia Cherepanov <cherepanov.ilya@gmail.com>
2017-04-19 21:59:37 +0700

    + Финальная версия ДЗ 7

--- App/Widgets/admin_data_table/templates/actions.php
+++
@@ -1,2 +0,0 @@
-<a href="/admin/delete-article/?id=<?php echo $article->id; ?>" class="btn btn-danger">Удалить</a>
-<a href="/admin/update-article/?id=<?php echo $article->id; ?>" class="btn btn-primary">Редактировать</a>
--- App/Widgets/admin_data_table/templates/table.php
+++ App/Widgets/admin_data_table/templates/table.php
@@ -5,6 +5,8 @@
             <?php foreach ($columns as $column): ?>
                 <td><?php echo $column($row); ?></td>
             <?php endforeach; ?>
+            <td><a href="/admin/delete-article/?id=<?php echo $row->id; ?>" class="btn btn-danger">Удалить</a></td>
+            <td><a href="/admin/update-article/?id=<?php echo $row->id; ?>" class="btn btn-primary">Редактировать</a></td>
         </tr>
     <?php endforeach; ?>
     </tbody>
